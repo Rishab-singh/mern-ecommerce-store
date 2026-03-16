a4c239f48f367931b356586fb1fe7181c641a8d8
rishab_demo <rishabsingh5085@gmail.com>
2026-03-16 13:09:27 +0530

    update api url for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://mern-ecommerce-store-t8vf.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
